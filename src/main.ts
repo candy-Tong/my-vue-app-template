8b87a7159f5283b628fd145bcbcc1334151115f4
candy-Tong <563378816@qq.com>
2022-01-15 16:16:04 +0800

    feat: 加入windicss

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp, ref } from 'vue';
 import App from './App.vue';
 import { router } from './router';
 
+import 'virtual:windi.css';
+import 'virtual:windi-devtools';
+
 const app = createApp(App);
 app.config.globalProperties.$loading = {
   fatherLoading: ref(false),
--- src/pages/HelloWorld.vue
+++ src/pages/HelloWorld.vue
@@ -1,11 +1,9 @@
 <template>
-  <h1>count: {{ count }}</h1>
+  <h1 class="mt-20px">count: {{ count }}</h1>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-
-defineProps<{ msg: string }>();
 
 const count = ref(0);
 </script>
